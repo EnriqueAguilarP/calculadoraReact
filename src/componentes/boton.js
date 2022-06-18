425394ecdf90c056d6b7e6f65bbbe7a345c7f7b3
EnriqueAguilarP <dialectikos@gmail.com>
2022-06-18 09:23:19 -0500

    Hoja de estilos para componente boton

--- src/componentes/boton.js
+++ src/componentes/boton.js
@@ -1,11 +1,12 @@
 import React from "react";
+import  '../hojaEstilos/boton.css'
 function Boton(props){
 
     const esOperador = valor => {
         return isNaN(valor) && (valor !== '.') && (valor !== '=')
     };
     return(
-        <div className={ `boton-contenedor ${ esOperador(props.children) ? 'operador' : null} `}>
+        <div className={ `boton-contenedor ${ esOperador(props.children) ? 'operador' : ""} `.trimEnd()}>
             {props.children}
         </div>
     );
